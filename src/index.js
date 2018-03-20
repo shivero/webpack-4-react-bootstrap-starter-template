fe9832a4ba8e6c98f066ee242293a8377c0d4b53
Grzegorz Dziubek <grzesiekdziubek@gmail.com>
2018-03-20 19:52:29 +0100

    build commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 // uncomment this line and check console after running npm run start
-console.log("Random number is: ", +Math.random().toFixed(2));
+// console.log("Random number is: ", +Math.random().toFixed(2));
