bfa75fdf9ebb41055bb7eb9cf32149ccc74be0fd
sh1vero <grzesiekdziubek@gmail.com>
2018-07-27 16:19:10 +0200

    Updated dependencies and resolved security issues

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-
-
 const arr = [1, 2, 3];
 const result = () => console.log(...arr);
 result();
 
-import bsStyles from 'bootstrap/dist/css/bootstrap.min.css';
-import appStyles from './styles/styles.scss';
+import bsStyles from "bootstrap/dist/css/bootstrap.min.css";
+import appStyles from "./styles/styles.scss";
 
-import 'bootstrap';
+import "bootstrap";
 import App from "./App";
